512d9f9cfb1b993945c67b47194e3c8ade4d433d
Nikhil Chhabra <91406971+procrastinatedbread@users.noreply.github.com>
2023-06-23 11:05:22 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+
+<div class="card">
+<h1>This is a Heading</h1>
+<h3>This is a sub-heading</h3>
+<img src="https://picsum.photos/500/400"/>
+ <p> We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel </p>
+  
+</div>
+`;
